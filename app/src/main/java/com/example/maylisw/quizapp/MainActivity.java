8de961f2554a5b2b307925066ab533b7d758be56
maylisw <maylisw31@gmail.com>
2017-09-19 12:38:47 -0700

    #5 added colors and wired widgets

--- app/src/main/java/com/example/maylisw/quizapp/MainActivity.java
+++ app/src/main/java/com/example/maylisw/quizapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.maylisw.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button trueButton, falseButton, nextButton;
+    private TextView question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wireWidgets();
     }
+    public void wireWidgets(){
+        trueButton = (Button) findViewById(R.id.button_true);
+        falseButton = (Button) findViewById(R.id.button_true);
+        nextButton = (Button) findViewById(R.id.button_true);
+        question = (TextView) findViewById(R.id.question_text);
+    }
+
 }
