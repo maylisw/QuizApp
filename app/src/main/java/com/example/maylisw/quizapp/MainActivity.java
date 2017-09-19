a0169f6c37a45b0a49f8a89d218a33e3753fb9bc
maylisw <maylisw31@gmail.com>
2017-09-19 13:29:05 -0700

    #6 started preliminary draft of questions

--- app/src/main/java/com/example/maylisw/quizapp/MainActivity.java
+++ app/src/main/java/com/example/maylisw/quizapp/MainActivity.java
@@ -2,18 +2,42 @@ package com.example.maylisw.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button trueButton, falseButton, nextButton;
     private TextView question;
+    private ArrayList<Questions> questions;
+    private int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         wireWidgets();
+        listenersOnClick();
+        createQuestions();
+        x = 0;
     }
+
+    private void createQuestions() {
+        questions = new ArrayList<>();
+        questions.add(new Questions("Le Tour de France occurs twice per yer.", false));
+        questions.add(new Questions("France is better than The United States of America in every way.", true));
+        questions.add(new Questions("The Vatican City is the capital the United States.", false));
+        questions.add(new Questions("France is the world's capital of posthumous marriage", true));
+        questions.add(new Questions("Isabelle is the best little sister", true));
+    }
+
+    private void listenersOnClick() {
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+    }
+
     public void wireWidgets(){
         trueButton = (Button) findViewById(R.id.button_true);
         falseButton = (Button) findViewById(R.id.button_true);
@@ -21,4 +45,24 @@ public class MainActivity extends AppCompatActivity {
         question = (TextView) findViewById(R.id.question_text);
     }
 
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button_false:
+                checkAnswer(x, false);
+                break;
+            case R.id.button_true:
+                checkAnswer(x, true);
+                break;
+            case R.id.button_next:
+                question.setText((CharSequence) questions.get(x));
+                x++;
+                break;
+        }
+    }
+
+    private void checkAnswer(int questionsNumber, boolean answer) {
+        questions.get(questionsNumber).checkAnswer(answer);
+    }
 }
